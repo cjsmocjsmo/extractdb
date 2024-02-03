642260b2cca658952d848dfdd6b1976aeb0453b6
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-02-02 22:11:34 -0800

    Update file paths for image directories

--- src/envvars.rs
+++ src/envvars.rs
@@ -7,15 +7,15 @@ pub fn set_env_vars() {
     };
     let dedups_jpg = env::var("DEDUPS_JPG");
     if dedups_jpg.is_err() {
-        env::set_var("DEDUPS_JPG", "/home/pipi/USB01/JPG");
+        env::set_var("DEDUPS_JPG", "/media/pipi/USB01/JPG");
     };
     let dedups_png = env::var("DEDUPS_PNG");
     if dedups_png.is_err() {
-        env::set_var("DEDUPS_PNG", "/home/pipi/USB01/PNG");
+        env::set_var("DEDUPS_PNG", "/media/pipi/USB01/PNG");
     };
     let dedups_bmp = env::var("DEDUPS_BMP");
     if dedups_bmp.is_err() {
-        env::set_var("DEDUPS_BMP", "/home/pipi/USB01/BMP");
+        env::set_var("DEDUPS_BMP", "/media/pipi/USB01/BMP");
     };
     let unable_to_open = env::var("UNABLE_TO_OPEN");
     if unable_to_open.is_err() {
