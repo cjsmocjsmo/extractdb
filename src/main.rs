3658f3761a2fb40bea1d2498340a366ca4a3481b
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-02-03 05:03:24 -0800

    Refactor image hash retrieval queries

--- src/main.rs
+++ src/main.rs
@@ -20,25 +20,24 @@ fn main() {
     let mut rows = stmt_jpg.query([]).unwrap();
     while let Some(row) = rows.next().unwrap() {
         let imghash1: String = row.get(0).unwrap();
-        
         img_hash_vec.push(imghash1);
     };
+    
+
+    let mut stmt_png = conn.prepare("SELECT DISTINCT imghash FROM pngs;").unwrap();
+    let mut rows = stmt_png.query([]).unwrap();
+    while let Some(row) = rows.next().unwrap() {
+        let imghash3: String = row.get(0).unwrap();
+        img_hash_vec.push(imghash3);
+    };
+
+    let mut stmt_bmp = conn.prepare("SELECT DISTINCT imghash FROM bmps;").unwrap();
+    let mut rows = stmt_bmp.query([]).unwrap();
+    while let Some(row) = rows.next().unwrap() {
+        let imghash2: String = row.get(0).unwrap();
+        img_hash_vec.push(imghash2);
+    };
     println!("img_hash_vec: {:#?}", img_hash_vec);
-
-    // let mut stmt_png = conn.prepare("SELECT DISTINCT imghash FROM pngs;").unwrap();
-    // let mut rows = stmt_png.query([]).unwrap();
-    // while let Some(row) = rows.next().unwrap() {
-    //     let imghash3: String = row.get(2).unwrap();
-    //     img_hash_vec.push(imghash3);
-    // };
-
-    // let mut stmt_bmp = conn.prepare("SELECT DISTINCT imghash FROM bmps;").unwrap();
-    // let mut rows = stmt_bmp.query([]).unwrap();
-    // while let Some(row) = rows.next().unwrap() {
-    //     let imghash2: String = row.get(2).unwrap();
-    //     img_hash_vec.push(imghash2);
-    // };
-
     // let jpg_out = env::var("DEDUPS_JPG").expect("DEDUPS_JPG not set");
     // fs::create_dir_all(jpg_out.clone()).expect("Unable to create JPG directory");
     // let png_out = env::var("DEDUPS_PNG").expect("DEDUPS_PNG not set");
