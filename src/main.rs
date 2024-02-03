a93ca25e09f6bcd5906d55eeaedc6d0b21307679
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-02-03 04:50:08 -0800

    Refactor image hash retrieval queries and update variable names

--- src/main.rs
+++ src/main.rs
@@ -15,47 +15,28 @@ fn main() {
     // let apath = &args[1];
     let db_path = env::var("DUPS_DB").expect("DUPS_DB not set");
     let conn = rusqlite::Connection::open(db_path).expect("Unable to open database");
-    let mut stmt_jpg = conn.prepare("SELECT * FROM jpgs;").unwrap();
+    let mut stmt_jpg = conn.prepare("SELECT DISTINCT imghash FROM jpgs;").unwrap();
     let mut rows = stmt_jpg.query([]).unwrap();
-    let mut old_img_path_vec = Vec::new();
+    let mut img_hash_vec = Vec::new();
     while let Some(row) = rows.next().unwrap() {
-        let imgid: String = row.get(1).unwrap();
-        let imghash: String = row.get(2).unwrap();
-        let imgpath: String = row.get(3).unwrap();
-        let meta = types::Meta {
-            imgid: imgid,
-            imghash: imghash,
-            imgpath: imgpath,
-        };
-        old_img_path_vec.push(meta);
+        
+        let imghash1: String = row.get(2).unwrap();
+        
+        img_hash_vec.push(imghash1);
     };
 
-    let mut stmt_png = conn.prepare("SELECT * FROM pngs;").unwrap();
+    let mut stmt_png = conn.prepare("SELECT DISTINCT imghash FROM pngs;").unwrap();
     let mut rows = stmt_png.query([]).unwrap();
     while let Some(row) = rows.next().unwrap() {
-        let imgid: String = row.get(1).unwrap();
-        let imghash: String = row.get(2).unwrap();
-        let imgpath: String = row.get(3).unwrap();
-        let meta = types::Meta {
-            imgid: imgid,
-            imghash: imghash,
-            imgpath: imgpath,
-        };
-        old_img_path_vec.push(meta);
+        let imghash3: String = row.get(2).unwrap();
+        img_hash_vec.push(imghash3);
     };
 
-    let mut stmt_bmp = conn.prepare("SELECT * FROM bmps;").unwrap();
+    let mut stmt_bmp = conn.prepare("SELECT DISTINCT imghash FROM bmps;").unwrap();
     let mut rows = stmt_bmp.query([]).unwrap();
     while let Some(row) = rows.next().unwrap() {
-        let imgid: String = row.get(1).unwrap();
-        let imghash: String = row.get(2).unwrap();
-        let imgpath: String = row.get(3).unwrap();
-        let meta = types::Meta {
-            imgid: imgid,
-            imghash: imghash,
-            imgpath: imgpath,
-        };
-        old_img_path_vec.push(meta);
+        let imghash2: String = row.get(2).unwrap();
+        img_hash_vec.push(imghash2);
     };
 
     let jpg_out = env::var("DEDUPS_JPG").expect("DEDUPS_JPG not set");
@@ -65,10 +46,56 @@ fn main() {
     let bmp_out = env::var("DEDUPS_BMP").expect("DEDUPS_BMP not set");
     fs::create_dir_all(bmp_out.clone()).expect("Unable to create BMP directory");
 
-    for e in old_img_path_vec {
+    let mut new_meta_vec = Vec::new();
+    for ihash in img_hash_vec {
         // let old_path = e.path().to_string_lossy().to_string();
-        println!("old_path: {}", e.imgpath);
-        let old_path = e.imgpath;
+        println!("old_path: {}", ihash);
+        let mut stmt = conn.prepare("SELECT * FROM jpgs WHERE imghash = ? LIMIT 1").unwrap();
+        let mut rows = stmt.query(&[&ihash]).unwrap();
+        
+        while let Some(row) = rows.next().unwrap() {
+            let imgidz = row.get(1).unwrap();
+            let imghashz = row.get(2).unwrap();
+            let imgpathz = row.get(3).unwrap();
+            let meta = types::Meta{
+                imgid: imgidz,
+                imghash: imghashz,
+                imgpath: imgpathz,
+            };
+            new_meta_vec.push(meta);
+        };
+
+        let mut stmt2 = conn.prepare("SELECT * FROM pngs WHERE imghash = ? LIMIT 1").unwrap();
+        let mut rows = stmt2.query(&[&ihash]).unwrap();
+        while let Some(row) = rows.next().unwrap() {
+            let imgidz = row.get(1).unwrap();
+            let imghashz = row.get(2).unwrap();
+            let imgpathz = row.get(3).unwrap();
+            let meta2 = types::Meta{
+                imgid: imgidz,
+                imghash: imghashz,
+                imgpath: imgpathz,
+            };
+            new_meta_vec.push(meta2);
+        };
+
+        let mut stm3 = conn.prepare("SELECT * FROM bmps WHERE imghash = ? LIMIT 1").unwrap();
+        let mut rows = stm3.query(&[&ihash]).unwrap();
+        while let Some(row) = rows.next().unwrap() {
+            let imgidz = row.get(1).unwrap();
+            let imghashz = row.get(2).unwrap();
+            let imgpathz = row.get(3).unwrap();
+            let meta3 = types::Meta{
+                imgid: imgidz,
+                imghash: imghashz,
+                imgpath: imgpathz,
+            };
+            new_meta_vec.push(meta3);
+        };
+    };
+
+    for meta in new_meta_vec {
+        let old_path = meta.imgpath;
         let fname = split_fname(old_path.clone());
         let ext = split_ext(old_path.clone());
 
